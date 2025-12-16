9a29237a071d39ecc259438343abbb05040f9a63
Samiul Islam Sami <isamiul797@gmail.com>
2025-12-16 09:00:45 +0600

    project4

---
+++ Consoleproject1/project4.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest: "))
+time = float(input("Enter the time(In yera): "))
+
+interest = (principal * rate * time) / 100
+
+print(f"The Totall Amount Of Interest: {interest}")
