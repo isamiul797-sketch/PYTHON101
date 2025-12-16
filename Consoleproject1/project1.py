fbd74fd1c09bf9d2a2835ef7dab1bacbac417b38
Samiul Islam Sami <isamiul797@gmail.com>
2025-12-16 08:31:53 +0600

    first commit

---
+++ Consoleproject1/project1.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name: ")
+age = int(input("Enter Your Age: "))
+address = input("Enter Your Address: ")
+
+print(f"Hey, I am {name}. And I am {age} years old. I am from {address}.Thank You!")
