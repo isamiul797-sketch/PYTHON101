5822c64e99de0b38000c0915c21ad44636b48d95
Samiul Islam Sami <isamiul797@gmail.com>
2025-12-19 10:58:40 +0600

    control flow

---
+++ controlFlow/if_else.py
@@ -0,0 +1,20 @@
+# age = 20
+#
+# if age >= 18:
+#     print("You are old enough to vote")
+# else:
+#     print("You aren't old enough to vote")
+
+#
+
+age = 20
+has_permission = False
+
+if age >= 18:
+    if has_permission:
+        print("you can Enter in club")
+    else:
+        print("you can't enter in club")
+
+else:
+    print("you can't enter in club")
