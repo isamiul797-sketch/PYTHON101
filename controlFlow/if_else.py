b01ac3e2270b113d5351b52a4d25d5765dff6a96
Samiul Islam Sami <isamiul797@gmail.com>
2025-12-19 11:01:37 +0600

    if elif else uses

--- controlFlow/if_else.py
+++ controlFlow/if_else.py
@@ -5,7 +5,29 @@
 # else:
 #     print("You aren't old enough to vote")
 
+# try:
+#     mark = int(input("Enter the mark (0-100): "))
 #
+#     # Validate mark range
+#     if mark < 0 or mark > 100:
+#         print("Invalid mark! Please enter a value between 0 and 100.")
+#     elif mark >= 80:
+#         print("A+")
+#     elif mark >= 70:
+#         print("A")
+#     elif mark >= 60:
+#         print("A-")
+#     elif mark >= 50:
+#         print("B")
+#     elif mark >= 40:
+#         print("C")
+#     elif mark >= 33:
+#         print("D")
+#     else:
+#         print("F")
+#
+# except ValueError:
+#     print("Invalid input! Please enter an integer.")
 
 age = 20
 has_permission = False
