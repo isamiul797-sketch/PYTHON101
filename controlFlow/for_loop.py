435a3f9374c7a863f3c62e14359044940981082c
Samiul Islam Sami <isamiul797@gmail.com>
2025-12-19 11:18:40 +0600

    for loop uses

---
+++ controlFlow/for_loop.py
@@ -0,0 +1,28 @@
+# fruits = ["apple", "banana", "cherry"]
+#
+# for fruit in fruits:
+#     print(fruit)
+
+
+# word = input("Enter a word: ")
+# for letter in word:
+#     print(letter)
+
+
+# for i in range(5,100,10):
+#     print(i)
+
+#
+# examresult = {'phy':70, 'chem':90, 'math':87}
+# for sub,marks in examresult.items():
+#     print("SUBJECT : ",sub)
+#     print("MARKS : ",marks)
+
+#
+# for num in range(1,11):
+#     if num == 5:
+#         break
+#         continue
+#     print(num)
+
+
