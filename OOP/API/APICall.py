c49105eed371bc93a35b032adf434723e084936f
Samiul Islam Sami <isamiul797@gmail.com>
2026-01-01 14:51:29 +0600

    api calling in raw python

---
+++ OOP/API/APICall.py
@@ -0,0 +1,26 @@
+# import requests
+
+#GET REQUEST
+# print("calling...")
+# res = requests.get("https://crud.teamrabbil.com/api/v1/ReadProduct")
+# print(res.status_code)
+# print(res.json())
+
+
+#POST REQUEST
+
+# URL = "https://crud.teamrabbil.com/api/v1/CreateProduct"
+
+# dict_data = {
+#     "ProductName": "Samsung",
+#     "ProductCode": "2018",
+#     "Img": "phone 1",
+#     "UnitPrice": "90000",
+#     "Qty": "9",
+#     "TotalPrice": "900000"
+# }
+
+# res = requests.post(url=URL, json=dict_data)
+
+# print(res.status_code)
+# print(res.json())
---
+++ OOP/API/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+print("calling...")
+# res = requests.get("https://crud.teamrabbil.com/api/v1/ReadProduct")
+# print(res.status_code)
+# print(res.json())
